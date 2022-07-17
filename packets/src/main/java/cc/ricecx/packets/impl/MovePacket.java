6b5b2fcc61523a0a7a1020ad1633653775f4051a
RiceCX <andyl5463@gmail.com>
2022-07-17 15:44:37 -0700

    example move packet

--- packets/src/main/java/cc/ricecx/packets/impl/MovePacket.java
+++ packets/src/main/java/cc/ricecx/packets/impl/MovePacket.java
@@ -1,16 +1,29 @@
 package cc.ricecx.packets.impl;
 
 import cc.ricecx.packets.Packet;
+import cc.ricecx.packets.Packets;
 
 public class MovePacket extends Packet<MovePacket> {
 
+    private int x;
+    private int y;
+    private int z;
+
     @Override
     public MovePacket deserialize(byte[] data) {
-        return null;
+        this.x = data[1];
+        this.y = data[2];
+        this.z = data[3];
+        return this;
     }
 
     @Override
     public byte[] serialize() {
-        return new byte[0];
+        byte[] data = new byte[128];
+        data[0] = (byte) Packets.MOVE.getId();
+        data[1] = (byte) this.x;
+        data[2] = (byte) this.y;
+        data[3] = (byte) this.z;
+        return data;
     }
 }
